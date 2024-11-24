d967a19e0cd7c4a453b2b0055cb4b036368de021
terraelfi <123456789+elfishahrin@gmail.com>
2024-11-24 13:10:32 +0800

    transfer script to script.js

---
+++ script.js
@@ -0,0 +1,94 @@
+function getComputerChoice() {
+    const randomNum = Math.random();
+
+    if (randomNum <= 0.33) {
+        return "Rock";
+    } else if (randomNum <= 0.66) {
+        return "Paper";
+    } else {
+        return "Scissor";
+    }
+};
+
+function getHumanChoice() {
+    let playerChoice
+    
+    while (true) {
+        playerChoice = prompt("Please type between ROCK, PAPER OR SCISSOR").toUpperCase();
+
+        if (playerChoice === "ROCK") {
+            return "Rock";
+        } else if (playerChoice === "PAPER") {
+            return "Paper";
+        } else if (playerChoice === "SCISSOR") {
+            return "Scissor"
+        } else {
+            alert("Invalid input. Please type Rock, Paper, or Scissor.");
+            return getHumanChoice();
+        }
+    }
+};
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return "DRAW!"
+    } else if (humanChoice === "Rock" && computerChoice === "Paper") {
+        return "CPU Wins! Because Paper beats Rock"
+        return computerScore++;
+    } else if (humanChoice === "Rock" && computerChoice === "Scissor") {
+        return "Player Wins! Because Rock beats Scissor"
+        return humanScore++;
+    } else if (humanChoice === "Scissor" && computerChoice === "Paper") {
+        return "Player Wins! Because Scissor beats Paper"
+        return humanScore++;
+    } else if (humanChoice === "Scissor" && computerChoice === "Rock") {
+        return "CPU Wins! Because Rock beats Scissor"
+        return computerScore++;
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        return "Player Wins! Because Paper beats Rock"
+        return humanScore++;
+    } else if (humanChoice === "Paper" && computerChoice === "Scissor") {
+        return "CPU Wins! Because Scissor beats Paper"
+        return computerScore++;
+    }
+};
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let gameRounds = 0;
+
+    for (gameRounds = 1; gameRounds <=5; gameRounds++) {
+        console.log(`Round ${gameRounds}`);
+        
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+
+        let result = playRound(humanChoice, computerChoice);
+        console.log(result);
+
+        // includes = find exact string of that
+        if (result.includes('Player Wins!')) {
+            humanScore++;
+        } else if (result.includes('CPU Wins!')) {
+            computerScore++;
+        }
+
+        console.log("Player picked: "+ humanChoice + 
+        "\nCPU picked: " + computerChoice + 
+        "\n=============" +
+        "\nHuman Score: " + humanScore +
+        "\nCPU Score: " + computerScore);
+    }
+
+    if (humanScore > computerScore){
+            console.log(`😃 YOU WIN! \nFINAL SCORE: HUMAN:${humanScore} - CPU:${computerScore}`)
+        } else if (computerScore > humanScore) {
+            console.log(`🤖 COMPUTER WIN! \nFINAL SCORE: HUMAN:${humanScore} - CPU:${computerScore}`)
+        } else {
+            console.log(`😮‍💨 DRAW! \nFINAL SCORE: HUMAN:${humanScore} - CPU:${computerScore}`)
+        }
+
+}
+
+playGame();
